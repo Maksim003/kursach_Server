d6d986832a20df63d173dc536f2a1963d3485d52
Maksim003 <savchik00@list.ru>
2024-11-25 19:33:50 +0300

    changes

--- src/main/java/com/example/kursach_Server/Database/Config.java
+++ src/main/java/com/example/kursach_Server/Database/Config.java
@@ -4,6 +4,5 @@ public class Config {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    //protected String dbPass = "tankdollar";
     protected String dbName = "kursach_psp";
 }
